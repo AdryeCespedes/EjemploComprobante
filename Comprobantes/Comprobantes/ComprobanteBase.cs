c5512fab02a48254fa05976f6ba7a457192fc739
Adrye Céspedes <adryec@gmail.com>
2022-01-25 15:50:41 -0300

    Agregamos clase de ítems. Generamos ComprobanteVenta y ComprobanteExportacion. Renombramos algunas clases.

--- Comprobantes/Comprobantes/ComprobanteBase.cs
+++ Comprobantes/Comprobantes/ComprobanteBase.cs
@@ -6,18 +6,18 @@ using System.Threading.Tasks;
 
 namespace Comprobantes
 {
-    public class BaseComprobanteID
+    public class ComprobanteID
     {
         public string Prefijo { get; set; } = String.Empty;
         public int Tipo { get; set; } = 0;
         public long Numero { get; set; } = 0;
         public DateOnly  Fecha { get; set; } = DateOnly.MinValue;
         public string Timbrado { get; set; } = String.Empty;
-        public DateOnly  FechaInicioTimbrado { get; set; } = DateOnly.MinValue;
+        public DateOnly FechaInicioTimbrado { get; set; } = DateOnly.MinValue;
         public string SerieTimbrado { get; set; } = String.Empty;
     }
 
-    public class BaseEmisor
+    public class ComprobanteEmisor
     {
         public string NroDocumento { get; set; } = String.Empty;
         public string RazonSocial { get; set; } = String.Empty;
@@ -29,9 +29,11 @@ namespace Comprobantes
         public string Telefono { get; set; } = String.Empty;
         public string SucursalCodigo { get; set; } = String.Empty;
         public string SucursalDescripcion { get; set; } = String.Empty;
+
+        public List<DatoAdicional>? DatosAdicionales { get; set; } = null;
     }
 
-    public class BaseReceptor
+    public class ComprobanteReceptor
     {
         public string TipoDocumento { get; set; } = String.Empty;
         public string NroDocumento { get; set; } = String.Empty;
@@ -46,7 +48,7 @@ namespace Comprobantes
         public string GLN { get; set; } = String.Empty;
         public string NroOrden { get; set; } = String.Empty;
 
-        public  List<DatoAdicional> DatosAdicionales { get; set; } = new List<DatoAdicional>(); 
+        public  List<DatoAdicional>? DatosAdicionales { get; set; } = null; 
     }
 
     public class DatoAdicional
@@ -61,7 +63,7 @@ namespace Comprobantes
     { 
           CuentaCorriente,
           Contado,
-          PagoDiferido
+          PagoDiferido,
           RendicionGastos
     }
 
@@ -71,43 +73,42 @@ namespace Comprobantes
         public metodosPago  MetodoPago  { get; set; } =  metodosPago.CuentaCorriente;
         public DateOnly FechaVtoPago { get; set; } = DateOnly.MinValue;
         public uint Moneda { get; set; } = 0;
-        public float  Cambio { get; set; } = 1;
+        public double  Cambio { get; set; } = 1;
         public string Observaciones  { get; set; } =  String.Empty;
     }
 
     public class DetalleIVA
     {
-        public float IVAtasa { get; set; } =  0;
-        public float IVAbaseImponible { get; set; } = 0;
-        public float IVAimporte { get; set; } = 0;
+        public double? IVATasa { get; set; }=null;
+        public double IVABaseImponible { get; set; } = 0;
+        public double IVAImporte { get; set; } = 0;
     }
 
     public enum OtrosTributosTipo
     { 
         IVA, 
         Ganancias, 
-        Ingresos Brutos,
+        IngresosBrutos,
         SUSS,
-        Impuestos Internos
+        ImpuestosInternos
     }
 
     public class DetalleOtrosTributos
     {
-        public OtrosTributosTipo OtrosTributostipo { get; set; } =  OtrosTributosTipo.IVA ;
-        public float OtrosTributosbaseimponible { get; set; } = 0;
-        public float OtrosTributosimporte { get; set; } = 0;
+        public OtrosTributosTipo? OtrosTributostipo { get; set; } =  null;
+        public double OtrosTributosbaseimponible { get; set; } = 0;
+        public double OtrosTributosimporte { get; set; } = 0;
     }
     public class ComprobanteTotales
     {
-        public float ImporteTotal { get; set; } = 0;
-        public float ImporteGravado { get; set; } = 0;
-        public float ImporteNoGravado { get; set; } = 0;
-        public float ImporteExento { get; set; } = 0;
-        public float ImporteIVANoInsc { get; set; } = 0;
-        public float ImporteRedondeo { get; set; } = 0;
-        public float ImporteRedondeo { get; set; } = 0;
-        public List<DetalleIVA> DetallesIVA { get; set; } = new List<DetalleIVA>(); 
-        public List<DetalleOtrosTributos> DetallesOtrosTributos { get; set; } = new List<DetalleOtrosTributos>(); 
+        public double ImporteTotal { get; set; } = 0;
+        public double ImporteGravado { get; set; } = 0;
+        public double ImporteNoGravado { get; set; } = 0;
+        public double ImporteExento { get; set; } = 0;
+        public double ImporteIVANoInsc { get; set; } = 0;
+        public double ImporteRedondeo { get; set; } = 0;
+        public List<DetalleIVA>? DetallesIVA { get; set; } = null; 
+        public List<DetalleOtrosTributos>? DetallesOtrosTributos { get; set; } = null; 
     }
 
     public enum tipoMedioPago
@@ -141,17 +142,17 @@ namespace Comprobantes
         public tipoMedioPago TipoMedioPago  { get; set; } = tipoMedioPago.Efectivo ;
         public string MedioPagoCodigo { get; set; } = String.Empty;
         public string MedioPagoDescripcion { get; set; } = String.Empty;
-        public float MedioPagoImporte { get; set; } = 0;
-        public float MedioPagoCantidad { get; set; } = 0;
+        public double MedioPagoImporte { get; set; } = 0;
+        public double MedioPagoCantidad { get; set; } = 0;
         public uint MedioPagoMoneda { get; set; } = 0;
-        public float MedioPagoCotizacion { get; set; } = 0;
+        public double MedioPagoCotizacion { get; set; } = 0;
         public Cheque Cheque { get; set; } = new Cheque ();
         public Tarjeta Tarjeta { get; set; } = new Tarjeta ();
     }
 
     public class ComprobanteMediosPago
     {
-        public List<MedioPago> MediosPago { get; set; } = new List<MedioPago>(); 
+        public List<MedioPago>? MediosPago { get; set; } = null; 
     }
 
     public enum soporte
@@ -172,13 +173,13 @@ namespace Comprobantes
     
     public class ComprobanteAsociados
     {
-        public List<ComprobanteAsociado> ComprobantesAsociado { get; set; } = new List<ComprobanteAsociado>(); 
+        public List<ComprobanteAsociado>? ComprobantesAsociado { get; set; } = null; 
     }
 
     public class ComprobantePeriodoAsociado
     {
-        public DateOnly  FechaDesde { get; set; } = DateOnly.MinValue;
-        public DateOnly  FechaHasta { get; set; } = DateOnly.MinValue;
+        public DateOnly FechaDesde { get; set; } = DateOnly.MinValue;
+        public DateOnly FechaHasta { get; set; } = DateOnly.MinValue;
     }
    
     public class Permiso
@@ -193,13 +194,86 @@ namespace Comprobantes
         public string ViaTransporte { get; set; } = String.Empty;
         public string PaisDestino { get; set; } = String.Empty;
         public uint Idioma { get; set; } = 1; 
-        public List<Permiso> Permisos { get; set; } = new List<Permiso>(); 
+        public List<Permiso>? Permisos { get; set; } = null; 
     }
 
-    /*public class Comprobante
+    public enum CondicionIVA
     {
-        public BaseComprobanteID ComprobanteID { get; set; } = new BaseComprobanteID ();
-        public ComprobanteCabeceraExportacion? ComprobanteCabeceraExportacion { get; set; };
+        Exento,
+        NoGravado,
+        Gravado
+    }
+    public class Item
+    { 
+        public string Unidad { get; set; } = String.Empty;
+        public string UnidadDesc { get; set; } = String.Empty;
 
-    }*/
+        public string CodigoMadre { get; set; } = String.Empty;
+        public string Codigo { get; set; } = String.Empty;
+        public string CodigoAlternativo { get; set; } = String.Empty;
+        public string Color { get; set; } = String.Empty;
+        public string Medida { get; set; } = String.Empty;
+        public string Descripcion { get; set; } = String.Empty;
+        public string DescripcionLarga { get; set; } = String.Empty;
+
+        public double? IVATasa { get; set; } = null;
+        public CondicionIVA? CondicionIVA { get; set; } = null;
+        public double Cantidad { get; set; } = 0;
+        public double PrecioUnitario { get; set; } = 0;
+        public double ImporteBonificacion { get; set; } = 0;
+        public double ImporteDescuento { get; set; } = 0;
+        public double ImporteRecargo { get; set; } = 0;
+        public double ImporteIVA { get; set; } = 0;
+        public double ImporteGravado { get; set; } = 0;
+        public bool EsRedondeo { get; set; } = false;
+        public List<DatoAdicional>? DatosAdicionales { get; set; } = null;
+        public List<DetalleOtrosTributos>? DetallesOtrosTributos { get; set; } = null;
+    }
+    public class ComprobanteItems
+    {
+        public List<Item>? Items { get; set; } = null;
+    }
+
+    public class ItemRem
+    {
+        public string Unidad1 { get; set; } = String.Empty;
+        public string Unidad2 { get; set; } = String.Empty;
+        public string CodigoMadre { get; set; } = String.Empty;
+        public string Codigo { get; set; } = String.Empty;
+        public string Color { get; set; } = String.Empty;
+        public string Medida { get; set; } = String.Empty;
+        public double Cantidad1 { get; set; } = 0;
+        public double Cantidad2 { get; set; } = 0;
+        public string Partida { get; set; } = String.Empty;
+    }
+
+    public class ComprobanteItemsRem{
+       public List<ItemRem>? Items { get; set; } = null;
+    }
+
+    public class ComprobanteVenta
+    {
+        public ComprobanteID Identificador { get; set; } = new ComprobanteID ();
+        public ComprobanteEmisor Emisor { get; set; } = new ComprobanteEmisor();
+        public ComprobanteReceptor Receptor { get; set; } = new ComprobanteReceptor();
+        public ComprobanteCabecera Cabecera { get; set; } = new ComprobanteCabecera();
+        public ComprobanteTotales Totales { get; set; } = new ComprobanteTotales();
+        public ComprobanteMediosPago? MediosPago { get; set; } = null;
+        public ComprobanteAsociados? Asociados { get; set; } = null;
+        public ComprobantePeriodoAsociado? PeriodoAsociado { get; set; } = null;    
+        public ComprobanteItems? Items { get; set; } = null;
+    }
+
+    public class ComprobanteExportacion
+    {
+        public ComprobanteID Identificador { get; set; } = new ComprobanteID();
+        public ComprobanteEmisor Emisor { get; set; } = new ComprobanteEmisor();
+        public ComprobanteReceptor Receptor { get; set; } = new ComprobanteReceptor();
+        public ComprobanteCabecera Cabecera { get; set; } = new ComprobanteCabecera();
+        public ComprobanteCabeceraExportacion CabeceraExportacion { get; set; } = new ComprobanteCabeceraExportacion();
+        public ComprobanteTotales Totales { get; set; } = new ComprobanteTotales();
+        public ComprobanteAsociados? Asociados { get; set; } = null;
+        public ComprobantePeriodoAsociado? PeriodoAsociado { get; set; } = null;
+        public ComprobanteItems Items { get; set; } = new ComprobanteItems();
+    }
 }
